af7632bea3c48644faf2da42658e37ea1998f5fc
msf <zwb1145@163.com>
2019-03-13 11:18:08 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style>
+::-webkit-scrollbar {
+  display: none;
+}
+</style>
+
+
