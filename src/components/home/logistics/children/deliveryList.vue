7bf031093c7c5de63f0e2d8d1d73cd4cfc9a9dcb
sjq <393762776@qq.com>
2019-07-22 17:46:30 +0800

    修改配送员列表删除弹出框

--- src/components/home/logistics/children/deliveryList.vue
+++ src/components/home/logistics/children/deliveryList.vue
@@ -121,7 +121,7 @@
         });
       }*/
       deleteItem(value) {
-        this.$confirm('此操作将永久删除此活动，是否继续？', '提示', {
+        this.$confirm('此操作将永久删除配送员，是否继续？', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
