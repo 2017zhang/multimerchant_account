b6decee0289300fb46a6acb57a46734a543bfbd1
gaopengch <1783268603@qq.com>
2020-01-10 10:22:32 +0800

    嗯嗯嗯

--- src/components/home/logistics/logistics.vue
+++ src/components/home/logistics/logistics.vue
@@ -28,7 +28,6 @@ export default {
     data (){
       return {
           title:'',
-        //   哈哈哈
           loading:true
         //   list:[{url:'shipAddress',title:'发货地址'},{url:'expressList',title:'快递公司列表'},{url:'freightTemplet',title:'运费模板'},{url:'freightFit',title:'运费设置'},],
       }
